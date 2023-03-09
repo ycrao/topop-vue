03d71069e8d781d04f429130b8ad36240a289e40
raoyc <raoyc@foxmail.com>
2023-03-09 19:07:20 +0800

    style: format

--- src/views/zhihu/hot.vue
+++ src/views/zhihu/hot.vue
@@ -1,31 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { fetchZhihuHot } from '@/api/zhihu'
-import { useRouter } from "vue-router";
+import { useRouter } from 'vue-router'
 const items = ref<object[]>([])
 const loading = ref(false)
 const finished = ref(false)
 
 const router = useRouter()
 const onLoad = () => {
-  fetchZhihuHot().then(resp => {
+  fetchZhihuHot().then((resp) => {
     items.value = resp.data
     loading.value = false
     finished.value = true
   })
 }
 const onClickLeft = () => {
-  router.push({path: '/'})
+  router.push({ path: '/' })
 }
 </script>
 
 <template>
   <div class="container">
     <div class="header">
-      <van-nav-bar title="知乎热榜"
-                   left-text="返回"
-                   left-arrow
-                   @click-left="onClickLeft" />
+      <van-nav-bar title="知乎热榜" left-text="返回" left-arrow @click-left="onClickLeft" />
     </div>
     <div class="itemContainer">
       <van-list
@@ -34,7 +31,13 @@ const onClickLeft = () => {
         finished-text="没有更多了"
         @load="onLoad"
       >
-        <van-cell v-for="item in items" :key="item.id" :title="item.title" :url="item.url" is-link />
+        <van-cell
+          v-for="item in items"
+          :key="item.id"
+          :title="item.title"
+          :url="item.url"
+          is-link
+        />
       </van-list>
     </div>
   </div>
